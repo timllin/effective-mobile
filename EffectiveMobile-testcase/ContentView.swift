3622a29801f754b3a38c7c6e70dde87093e3e026
Тимур Калимуллин <kalimullin.tt@phystech.edu>
2024-05-31 21:36:52 +0300

    Initial Commit

---
+++ EffectiveMobile-testcase/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  EffectiveMobile-testcase
+//
+//  Created by Тимур Калимуллин on 31.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EffectiveMobile-testcase/EffectiveMobile_testcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobile_testcaseApp.swift
+//  EffectiveMobile-testcase
+//
+//  Created by Тимур Калимуллин on 31.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobile_testcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
